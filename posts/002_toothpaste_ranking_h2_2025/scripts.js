8dce844e966d98f0b79cb06c12c80f13bdfa37ef
Mukund R B <mukundhaha@gmail.com>
2025-09-13 17:22:05 +0200

    Built site for gh-pages

--- posts/002_toothpaste_ranking_h2_2025/scripts.js
+++
@@ -1,9 +0,0 @@
-// Add scroll class to navbar when scrolling
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
