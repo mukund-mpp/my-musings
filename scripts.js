4f5f1c17c9334b6c69747a5bcf1633d8f547d93c
Mukund R B <mukundhaha@gmail.com>
2025-09-13 17:17:45 +0200

    Built site for gh-pages

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// Add scroll class to navbar when scrolling
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
